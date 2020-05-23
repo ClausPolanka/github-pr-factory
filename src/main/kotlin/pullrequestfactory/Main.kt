3c4759ba1a479db5fbbdc31f158a2fddf8e04282
ClausPolanka <claus.polanka@chello.at>
2020-05-23 18:43:44 +0200

    Add guard clause for program arguments

--- src/main/kotlin/pullrequestfactory/Main.kt
+++ src/main/kotlin/pullrequestfactory/Main.kt
@@ -5,6 +5,14 @@ import pullrequestfactory.domain.GithubPRFactory
 import pullrequestfactory.io.GithubHttpRepo
 
 fun main(args: Array<String>) {
+    when {
+        args.size != 2 -> println("java -jar <EXEC_JAR>.jar <CANDIDATE_FIRST_NAME>-<CANDIDATE_LAST_NAME> <BASIC_AUTH_TOKEN>")
+        !args[0].contains("-") -> println("Candidate first name and last name must be separated by hypen: <CANDIDATE_FIRST_NAME>-<CANDIDATE_LAST_NAME>")
+        else -> executeProgram(args)
+    }
+}
+
+fun executeProgram(args: Array<String>) {
     val candidateFullName = args[0]
     val c = Candidate(candidateFullName.split("-")[0], candidateFullName.split("-")[1])
     val f = GithubPRFactory(GithubHttpRepo("wordcount", basicAuth = args[1]))
