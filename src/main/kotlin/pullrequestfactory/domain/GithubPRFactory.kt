86ad703782c689c44fdd75a55b6ef92716d94862
ClausPolanka <claus.polanka@chello.at>
2020-05-23 16:36:51 +0200

    Support same session with different iterations and different sessions

--- src/main/kotlin/pullrequestfactory/domain/GithubPRFactory.kt
+++ src/main/kotlin/pullrequestfactory/domain/GithubPRFactory.kt
@@ -3,13 +3,16 @@ package pullrequestfactory.domain
 class GithubPRFactory(private val githubRepo: GithubRepo) {
     fun create_pull_requests_for_candidate(candidate: Candidate) {
         val branches = githubRepo.get_all_branches()
-        val branch = branches
                 .filter { it.name.contains(candidate.firstName, ignoreCase = true) }
-                .filter { it.name.contains(candidate.lastName, ignoreCase = true) }[0]
-        val (_, _, _, iterationNr, pairingPartner) = branch.name.split("_")
+                .filter { it.name.contains(candidate.lastName, ignoreCase = true) }
         branches.forEachIndexed { idx, br ->
+            val (_, _, _, iterationNr, pairingPartner) = br.name.split("_")
+            val sessionNr = when {
+                idx != 0 && branches[idx.dec()].name.endsWith(pairingPartner) -> idx
+                else -> idx.inc()
+            }
             githubRepo.create_pull_request(
-                    "${candidate.firstName} ${candidate.lastName} Iteration $iterationNr / Session ${idx.inc()} ${pairingPartner.capitalize()}")
+                    "${candidate.firstName} ${candidate.lastName} Iteration $iterationNr / Session $sessionNr ${pairingPartner.capitalize()}")
         }
     }
 }
--- src/test/kotlin/ut/pullrequestfactory/domain/GithubPRFactoryTest.kt
+++ src/test/kotlin/ut/pullrequestfactory/domain/GithubPRFactoryTest.kt
@@ -12,7 +12,7 @@ import pullrequestfactory.domain.GithubRepo
 class GithubPRFactoryTest {
 
     @Test
-    fun create_pull_requests_for_candidate() {
+    fun create_one_pull_request() {
         val githubRepo = mockk<GithubRepo>(relaxed = true)
         val sut = GithubPRFactory(githubRepo)
         val candidate = Candidate("Firstname", "Lastname")
@@ -24,6 +24,38 @@ class GithubPRFactoryTest {
         sut.create_pull_requests_for_candidate(candidate)
 
         verify { githubRepo.create_pull_request("Firstname Lastname Iteration 1 / Session 1 Claus") }
+    }
+
+    @Test
+    fun create_two_pull_requests_for_same_pairing_partner_but_different_iterations() {
+        val githubRepo = mockk<GithubRepo>(relaxed = true)
+        val sut = GithubPRFactory(githubRepo)
+        val candidate = Candidate("Firstname", "Lastname")
+
+        every {
+            githubRepo.get_all_branches()
+        } returns listOf(Branch("firstname_lastname_iteration_1_claus"), Branch("firstname_lastname_iteration_2_claus"))
+
+        sut.create_pull_requests_for_candidate(candidate)
+
+        verify { githubRepo.create_pull_request("Firstname Lastname Iteration 1 / Session 1 Claus") }
+        verify { githubRepo.create_pull_request("Firstname Lastname Iteration 2 / Session 1 Claus") }
+    }
+
+    @Test
+    fun create_two_pull_requests_for_same_iteration_but_different_sessions_and_pairing_partners() {
+        val githubRepo = mockk<GithubRepo>(relaxed = true)
+        val sut = GithubPRFactory(githubRepo)
+        val candidate = Candidate("Firstname", "Lastname")
+
+        every {
+            githubRepo.get_all_branches()
+        } returns listOf(Branch("firstname_lastname_iteration_1_claus"), Branch("firstname_lastname_iteration_1_berni"))
+
+        sut.create_pull_requests_for_candidate(candidate)
+
+        verify { githubRepo.create_pull_request("Firstname Lastname Iteration 1 / Session 1 Claus") }
+        verify { githubRepo.create_pull_request("Firstname Lastname Iteration 1 / Session 2 Berni") }
     }
 
 }
