a185eaace8e6aaa117f9ec252289d91c1477d6ca
ClausPolanka <claus.polanka@chello.at>
2020-05-24 10:50:52 +0200

    Extract method to improve readability

--- src/main/kotlin/pullrequestfactory/domain/BranchSorter.kt
+++ src/main/kotlin/pullrequestfactory/domain/BranchSorter.kt
@@ -5,17 +5,21 @@ class BranchSorter {
     fun sort_branches_by_pairing_partner(branches: List<Branch>, orderByPairingPartner: List<String>): List<Branch> {
         val sortedBranches = mutableListOf<List<Branch>>()
         orderByPairingPartner.forEach { pairingPartner ->
-            sortedBranches.add(branches
-                    .filter { it.name.endsWith(pairingPartner.toLowerCase()) }
-                    .map { branch ->
-                        val branchNameParts = branch.name.split("_")
-                        val iteration = branchNameParts.dropLast(1).last()
-                        Pair(branch.name, iteration)
-                    }
-                    .sortedBy { it.second }
-                    .map { Branch(it.first) })
+            sortedBranches.add(sort(branches, pairingPartner))
         }
         return sortedBranches.flatten()
+    }
+
+    private fun sort(branches: List<Branch>, pairingPartner: String): List<Branch> {
+        return branches
+                .filter { it.name.endsWith(pairingPartner.toLowerCase()) }
+                .map { branch ->
+                    val branchNameParts = branch.name.split("_")
+                    val iteration = branchNameParts.dropLast(1).last()
+                    Pair(branch.name, iteration)
+                }
+                .sortedBy { it.second }
+                .map { Branch(it.first) }
     }
 
 }
