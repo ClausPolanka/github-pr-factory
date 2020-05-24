8c23befc945ec1f04afd47f400c2ffe9e20bc26f
ClausPolanka <claus.polanka@chello.at>
2020-05-24 10:58:14 +0200

    Extract methods to separate concerns

--- src/main/kotlin/pullrequestfactory/domain/BranchSorter.kt
+++ src/main/kotlin/pullrequestfactory/domain/BranchSorter.kt
@@ -9,8 +9,12 @@ class BranchSorter {
     }
 
     private fun create_sorted_branches(branches: List<Branch>, pairingPartner: String): List<Branch> {
+        val branchesForPairingPartner = branches.filter { it.name.endsWith(pairingPartner.toLowerCase()) }
+        return sort_by_iteration_number(branchesForPairingPartner)
+    }
+
+    private fun sort_by_iteration_number(branches: List<Branch>): List<Branch> {
         return branches
-                .filter { it.name.endsWith(pairingPartner.toLowerCase()) }
                 .map { branch ->
                     val branchNameParts = branch.name.split("_")
                     val iteration = branchNameParts.dropLast(1).last()
