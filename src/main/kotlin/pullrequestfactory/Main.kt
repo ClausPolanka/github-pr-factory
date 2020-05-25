711e7499679ec8c955153a152b0b39b3c589e7e2
ClausPolanka <claus.polanka@chello.at>
2020-05-25 21:48:30 +0200

    Check if provided pairing partner are well known

--- src/main/kotlin/pullrequestfactory/Main.kt
+++ src/main/kotlin/pullrequestfactory/Main.kt
@@ -4,11 +4,16 @@ import pullrequestfactory.domain.Candidate
 import pullrequestfactory.domain.GithubPRFactory
 import pullrequestfactory.io.GithubHttpRepo
 
+val backendChapterTeamMembers = listOf("claus", "berni", "nandor", "dominik", "mihai", "lampe", "shubi", "markus", "tibor", "christian", "michal", "tomas", "peter", "martin", "john")
+
 fun main(args: Array<String>) {
     when {
         args.size != 3 -> println("java -jar <EXEC_JAR>.jar <CANDIDATE_FIRST_NAME>-<CANDIDATE_LAST_NAME> <BASIC_AUTH_TOKEN> <[PAIRING_PARTNER]>")
         !args[0].contains("-") -> println("Candidate first name and last name must be separated by hypen: <CANDIDATE_FIRST_NAME>-<CANDIDATE_LAST_NAME>")
-        args[2].split("-").size != 7 -> println("At least 7 pairing partner must be provided separated by hyphen")
+        args[2].split("-").size != 7 -> println("Exactly 7 pairing partner must be provided separated by hyphen")
+        args[2].split("-").filter {
+            backendChapterTeamMembers.contains(it)
+        }.size != 7 -> println("At least one provided pairing partner is unknown. Please check the list of provided pairing partner.")
         else -> executeProgram(args)
     }
 }
