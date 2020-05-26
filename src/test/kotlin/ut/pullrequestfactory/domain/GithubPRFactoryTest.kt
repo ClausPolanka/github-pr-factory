f04a739cd572b36defc8602fd253bb414842ff0c
ClausPolanka <claus.polanka@chello.at>
2020-05-26 12:09:37 +0200

    Improve formatting to improve readability

--- src/test/kotlin/ut/pullrequestfactory/domain/GithubPRFactoryTest.kt
+++ src/test/kotlin/ut/pullrequestfactory/domain/GithubPRFactoryTest.kt
@@ -16,11 +16,23 @@ class GithubPRFactoryTest {
 
         every {
             githubReadRepo.get_all_branches()
-        } returns listOf(Branch("firstname_lastname_iteration_1_claus"), Branch("firstname_lastname_iteration_2_berni"))
+        } returns listOf(
+                Branch("firstname_lastname_iteration_1_claus"),
+                Branch("firstname_lastname_iteration_2_berni"))
 
         sut.create_pull_requests(Candidate("Firstname", "Lastname"), listOf("Claus", "Berni"))
 
-        verify { githubWriteRepo.create_pull_request(PullRequest("Firstname Lastname Iteration 1 / Session 1 Claus [PR]", "master", "firstname_lastname_iteration_1_claus")) }
-        verify { githubWriteRepo.create_pull_request(PullRequest("Firstname Lastname Iteration 2 / Session 2 Berni", "firstname_lastname_iteration_1_claus", "firstname_lastname_iteration_2_berni")) }
+        verify {
+            githubWriteRepo.create_pull_request(PullRequest(
+                    title = "Firstname Lastname Iteration 1 / Session 1 Claus [PR]",
+                    base = "master",
+                    head = "firstname_lastname_iteration_1_claus"))
+        }
+        verify {
+            githubWriteRepo.create_pull_request(PullRequest(
+                    title = "Firstname Lastname Iteration 2 / Session 2 Berni",
+                    base = "firstname_lastname_iteration_1_claus",
+                    head = "firstname_lastname_iteration_2_berni"))
+        }
     }
 }
