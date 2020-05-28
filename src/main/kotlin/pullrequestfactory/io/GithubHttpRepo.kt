fb71b6d12935116e6fcad7b1de0fe9ab5b9bfd14
ClausPolanka <claus.polanka@chello.at>
2020-05-28 16:46:03 +0200

    Use if as expresson because of Kotlin

--- src/main/kotlin/pullrequestfactory/io/GithubHttpRepo.kt
+++ src/main/kotlin/pullrequestfactory/io/GithubHttpRepo.kt
@@ -16,13 +16,13 @@ class GithubHttpRepo(
             println("Too many requests to Github within time limit")
             return emptyList()
         }
+        cacheRepo.cache(response.text, pageNr = 1)
         return get_all_branches_for(response)
     }
 
     private fun get_all_branches_for(response: Response): List<Branch> {
         val lastPage = last_page_of_branches(response)
         val allBranches = mutableListOf<List<Branch>>()
-        cacheRepo.cache(response.text, pageNr = 1)
         allBranches.add(Klaxon().parseArray(response.text)!!)
         (2..lastPage.toInt()).forEach {
             val json = khttp.get("$baseUrl/repos/ClausPolanka/$repoName/branches?page=$it").text
@@ -45,12 +45,12 @@ class GithubHttpRepo(
     }
 
     private fun last_page_of_branches(response: Response): String {
-        if (response.headers["link"] == null) {
-            return "-1"
+        return if (response.headers["link"] == null) {
+            "-1"
         } else {
             val pattern = "page=([0-9]+)".toRegex()
             val matches = pattern.findAll(response.headers["link"] ?: "")
-            return matches.last().groupValues[1]
+            matches.last().groupValues[1]
         }
     }
 
