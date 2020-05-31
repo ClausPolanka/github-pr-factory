93e91ae291769d7b5740aaded9327011018f923b
ClausPolanka <claus.polanka@chello.at>
2020-05-31 13:23:13 +0200

    Remove mockk to improve test performance

--- src/test/kotlin/it/pullrequestfactory/io/ProgramArgsTest.kt
+++ src/test/kotlin/it/pullrequestfactory/io/ProgramArgsTest.kt
@@ -1,7 +1,5 @@
 package it.pullrequestfactory.io
 
-import io.mockk.mockk
-import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import pullrequestfactory.domain.UI
@@ -11,52 +9,53 @@ class ProgramArgsTest {
 
     @Test
     fun prints_error_message_for_empty_arguments() {
-        val ui = mockk<UI>(relaxed = true)
+        val ui = TestUI()
         val sut = ProgramArgs(emptyArray(), ui)
 
         sut.areValid()
         sut.printErrorMessage()
 
-        verify { ui.show(sut.programUsageMsg) }
+        assertThat(sut.programUsageMsg).isEqualTo(ui.shownMessage)
     }
 
     @Test
     fun prints_error_message_for_arguments_containing_only_one_element() {
-        val ui = mockk<UI>(relaxed = true)
+        val ui = TestUI()
         val sut = ProgramArgs(arrayOf("arg1"), ui)
 
         sut.areValid()
         sut.printErrorMessage()
 
-        verify { ui.show(sut.programUsageMsg) }
+        assertThat(sut.programUsageMsg).isEqualTo(ui.shownMessage)
     }
 
     @Test
     fun prints_error_message_for_arguments_containing_only_two_elements() {
-        val ui = mockk<UI>(relaxed = true)
+        val ui = TestUI()
         val sut = ProgramArgs(arrayOf("arg1", "arg2"), ui)
 
         sut.areValid()
         sut.printErrorMessage()
 
-        verify { ui.show(sut.programUsageMsg) }
+        assertThat(sut.programUsageMsg).isEqualTo(ui.shownMessage)
     }
 
     @Test
     fun prints_error_message_for_candidate_argument_not_having_the_correct_syntax() {
-        val ui = mockk<UI>(relaxed = true)
+        val ui = TestUI()
         val candidate = "arg1"
         val sut = ProgramArgs(arrayOf(candidate, "arg2", "arg3"), ui)
 
         sut.areValid()
         sut.printErrorMessage()
 
-        verify { ui.show(sut.candidateSyntaxMsg) }
+        assertThat(sut.candidateSyntaxMsg).isEqualTo(ui.shownMessage)
+
     }
 
     @Test
     fun prints_error_message_for_pairing_partner_argument_having_the_wrong_count() {
-        val ui = mockk<UI>(relaxed = true)
+        val ui = TestUI()
         val candidate = "firstname-lastname"
         val pairingPartner = "arg3"
         val sut = ProgramArgs(arrayOf(candidate, "arg2", pairingPartner), ui)
@@ -64,12 +63,12 @@ class ProgramArgsTest {
         sut.areValid()
         sut.printErrorMessage()
 
-        verify { ui.show(sut.nrOfRequiredPairingPartnerMsg) }
+        assertThat(sut.nrOfRequiredPairingPartnerMsg).isEqualTo(ui.shownMessage)
     }
 
     @Test
     fun prints_error_message_for_pairing_partner_argument_containing_at_least_one_unknown_member() {
-        val ui = mockk<UI>(relaxed = true)
+        val ui = TestUI()
         val candidate = "firstname-lastname"
         val pairingPartner = "pp1-pp2-pp3-pp4-pp5-pp6-pp7"
         val sut = ProgramArgs(arrayOf(candidate, "arg2", pairingPartner), ui)
@@ -77,19 +76,27 @@ class ProgramArgsTest {
         sut.areValid()
         sut.printErrorMessage()
 
-        verify { ui.show(sut.unknownPairingPartnerMsg) }
+        assertThat(sut.unknownPairingPartnerMsg).isEqualTo(ui.shownMessage)
     }
 
     @Test
     fun program_arguments_are_valid_if_all_arguments_are_provided_and_have_the_expected_syntax() {
-        val ui = mockk<UI>(relaxed = true)
         val candidate = "firstname-lastname"
         val pairingPartner = "claus-berni-mihai-nandor-lampe-christian-shubi"
-        val sut = ProgramArgs(arrayOf(candidate, "arg2", pairingPartner), ui)
+        val sut = ProgramArgs(arrayOf(candidate, "arg2", pairingPartner), TestUI())
 
         val areValid = sut.areValid()
 
         assertThat(areValid).isTrue()
+    }
+
+}
+
+class TestUI() : UI {
+    var shownMessage: String = ""
+
+    override fun show(msg: String) {
+        shownMessage = msg
     }
 
 }
