de89d392b67e1b3afd9bd447b1c210752048d6f3
ClausPolanka <claus.polanka@chello.at>
2020-06-01 17:25:26 +0200

    Fix typo

--- src/main/kotlin/pullrequestfactory/io/GithubHttpRepo.kt
+++ src/main/kotlin/pullrequestfactory/io/GithubHttpRepo.kt
@@ -5,14 +5,14 @@ import khttp.responses.Response
 import pullrequestfactory.domain.*
 
 class GithubHttpRepo(
-        private val baseUrli: String,
+        private val baseUrl: String,
         private val repoName: String,
         private val basicAuthToken: String,
         private val cacheRepo: CacheRepo,
         private val ui: UI) : GithubReadRepo, GithubWriteRepo {
 
     override fun get_all_branches(): List<Branch> {
-        val response = khttp.get("$baseUrli/repos/ClausPolanka/$repoName/branches?page=1")
+        val response = khttp.get("$baseUrl/repos/ClausPolanka/$repoName/branches?page=1")
         if (response.statusCode == 403) {
             ui.show("Too many requests to Github within time limit")
             return emptyList()
@@ -26,7 +26,7 @@ class GithubHttpRepo(
         val allBranches = mutableListOf<List<Branch>>()
         allBranches.add(Klaxon().parseArray(response.text)!!)
         (2..lastPage.toInt()).forEach {
-            val json = khttp.get("$baseUrli/repos/ClausPolanka/$repoName/branches?page=$it").text
+            val json = khttp.get("$baseUrl/repos/ClausPolanka/$repoName/branches?page=$it").text
             cacheRepo.cache(response.text, pageNr = it)
             allBranches.add(Klaxon().parseArray(json)!!)
         }
@@ -36,7 +36,7 @@ class GithubHttpRepo(
     override fun create_pull_request(pullRequest: PullRequest) {
         ui.show("Create pull request on Github: $pullRequest")
         val response = khttp.post(
-                url = "$baseUrli/repos/ClausPolanka/$repoName/pulls",
+                url = "$baseUrl/repos/ClausPolanka/$repoName/pulls",
                 headers = mapOf(
                         "Accept" to "application/json",
                         "Authorization" to "Basic $basicAuthToken",
