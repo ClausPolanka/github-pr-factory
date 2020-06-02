ad772a725a4ddf939a9a4676492320b17fc19ae6
ClausPolanka <claus.polanka@chello.at>
2020-06-02 16:58:11 +0200

    Use Java nio classes and reduce code complexity

--- src/main/kotlin/pullrequestfactory/io/Properties.kt
+++ src/main/kotlin/pullrequestfactory/io/Properties.kt
@@ -1,16 +1,18 @@
 package pullrequestfactory.io
 
+import java.nio.file.Files
+import java.nio.file.Paths
+
 class Properties(private val fileName: String) {
 
     private val indexOfValue = 1
     private val defaultUrl = "http://localhost"
 
     fun getBaseUrl(): String {
-        val fileContent = this::class.java.classLoader.getResource(fileName)?.readText()
-        return fileContent?.split("\n")
-                ?.filter { it.isNotEmpty() }
-                ?.first { it.startsWith("baseUrl") }
-                ?.split("=")?.get(indexOfValue) ?: defaultUrl
+        val url = this::class.java.classLoader.getResource(fileName) ?: return defaultUrl
+        return Files.readAllLines(Paths.get(url.toURI()))
+                .first { it.startsWith("baseUrl") }
+                .split("=")[indexOfValue]
     }
 
 }
