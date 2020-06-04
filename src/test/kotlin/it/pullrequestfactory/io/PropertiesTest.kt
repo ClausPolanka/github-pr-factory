37d509f432e18c8ba90c26a3d14e611690ecfa43
ClausPolanka <claus.polanka@chello.at>
2020-06-05 00:15:58 +0200

    Make tests self-contained

--- src/test/kotlin/it/pullrequestfactory/io/PropertiesTest.kt
+++ src/test/kotlin/it/pullrequestfactory/io/PropertiesTest.kt
@@ -1,15 +1,26 @@
 package it.pullrequestfactory.io
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
 import org.junit.Test
 import pullrequestfactory.io.ConsoleUI
 import pullrequestfactory.io.Properties
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class PropertiesTest {
 
+    private val fileName = "tmp.properties"
+
+    @After
+    fun tearDown() {
+        Files.deleteIfExists(Paths.get("target/test-classes/$fileName"))
+    }
+
     @Test
     fun get_base_url_from_given_file() {
-        val sut = Properties("app.properties", ConsoleUI())
+        val fileName = createPropsWith("baseUrl=http://localhost:8080")
+        val sut = Properties(fileName, ConsoleUI())
 
         val baseUrl = sut.getBaseUrl()
 
@@ -18,11 +29,17 @@ class PropertiesTest {
 
     @Test
     fun get_default_base_url_in_case_file_does_not_exist() {
-        val sut = Properties("invalid.txt", ConsoleUI())
+        val fileName = createPropsWith("x=y")
+        val sut = Properties(fileName, ConsoleUI())
 
         val baseUrl = sut.getBaseUrl()
 
         assertThat(baseUrl).isEqualTo("http://localhost")
+    }
+
+    private fun createPropsWith(prop: String): String {
+        Files.write(Paths.get("target/test-classes/$fileName"), prop.toByteArray())
+        return fileName
     }
 
 }
