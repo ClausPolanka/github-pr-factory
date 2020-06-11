6ce8cad88f0a0e4901c7a8c932052f3ff05a6119
ClausPolanka <claus.polanka@chello.at>
2020-06-11 18:03:43 +0200

    Rename method to better reveal intent

--- src/main/kotlin/pullrequestfactory/domain/PullRequest.kt
+++ src/main/kotlin/pullrequestfactory/domain/PullRequest.kt
@@ -5,10 +5,10 @@ data class PullRequest(private var _title: String, val base: String, val head: S
     val title: String get() = _title
 
     fun add_pr_mark_to_title(nextPr: PullRequest) {
-        _title = if (nextPr.is_pull_request()) "$_title [PR]" else _title
+        _title = if (nextPr.is_new_iteration()) "$_title [PR]" else _title
     }
 
-    private fun is_pull_request(): Boolean = !is_base_master() && Branch(base).iteration_nr() < Branch(head).iteration_nr()
+    private fun is_new_iteration(): Boolean = !is_base_master() && Branch(base).iteration_nr() < Branch(head).iteration_nr()
 
     private fun is_base_master(): Boolean = base == "master"
 
