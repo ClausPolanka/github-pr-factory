99ac2ee466a6c3f7c5c25dc6f465a582b7b16426
ClausPolanka <claus.polanka@chello.at>
2020-06-13 18:42:32 +0200

    Remove unnecessary method call

--- src/main/kotlin/pullrequestfactory/domain/BranchTitles.kt
+++ src/main/kotlin/pullrequestfactory/domain/BranchTitles.kt
@@ -7,7 +7,7 @@ class BranchTitles(private val branches: List<Branch>) {
         return branches.mapIndexed { idx, br ->
             val (firstName, lastName, _, iterationNr, pairingPartner) = br.parts()
             "${firstName.capitalize()} ${lastName.capitalize()} " +
-                    "Iteration $iterationNr / Session ${sessions[idx].toInt()} ${pairingPartner.capitalize()}"
+                    "Iteration $iterationNr / Session ${sessions[idx]} ${pairingPartner.capitalize()}"
         }
     }
 
