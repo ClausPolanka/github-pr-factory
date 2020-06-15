820cec509f368f1d2b3ba1f3834b5d594cbd7e93
ClausPolanka <claus.polanka@chello.at>
2020-06-15 17:28:53 +0200

    Remove unused file

--- src/main/kotlin/pullrequestfactory/domain/HeadBranches.kt
+++
@@ -1,9 +0,0 @@
-package pullrequestfactory.domain
-
-class HeadBranches(private val branches: List<Branch>) {
-
-    fun create(): List<String> {
-        return branches.map { it.name }
-    }
-
-}
