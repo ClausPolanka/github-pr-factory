49b79f29555b95066932c7f0f507530306f0acbf
ClausPolanka <claus.polanka@chello.at>
2020-07-06 20:57:11 +0200

    Change constants naming convention to idiomatic Kotlin

--- src/main/kotlin/pullrequestfactory/io/Properties.kt
+++ src/main/kotlin/pullrequestfactory/io/Properties.kt
@@ -2,17 +2,17 @@ package pullrequestfactory.io
 
 class Properties(fileName: String) {
 
+    private val DEFAULT_URL = "http://localhost"
     private val props = java.util.Properties()
-    private val defaultUrl = "http://localhost"
 
     init {
         props.load(this::class.java.classLoader.getResourceAsStream(fileName))
     }
 
-    fun get_github_base_url() = props.getProperty("baseUrl", defaultUrl)
+    fun get_github_base_url() = props.getProperty("baseUrl", DEFAULT_URL)
 
-    fun get_project_version() = props.getProperty("projectVersion", defaultUrl)
+    fun get_project_version() = props.getProperty("projectVersion", DEFAULT_URL)
 
-    fun get_github_repository_path() = props.getProperty("repoPath", defaultUrl)
+    fun get_github_repository_path() = props.getProperty("repoPath", DEFAULT_URL)
 
 }
