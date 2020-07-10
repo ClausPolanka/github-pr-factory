60e99de0b14cf569f1f0b0fdb9ff9a3fa201972a
ClausPolanka <claus.polanka@chello.at>
2020-07-10 09:50:29 +0200

    Rename unused variable to _

--- src/main/kotlin/pullrequestfactory/domain/branches/BaseBranches.kt
+++ src/main/kotlin/pullrequestfactory/domain/branches/BaseBranches.kt
@@ -3,7 +3,7 @@ package pullrequestfactory.domain.branches
 class BaseBranches(private val branches: List<Branch>) {
 
     fun create(): List<Branch> {
-        return branches.mapIndexed { idx, br ->
+        return branches.mapIndexed { idx, _ ->
             when (idx) {
                 0 -> Branch("master")
                 else -> branches[idx.dec()]
