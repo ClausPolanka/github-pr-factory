2e03e05d3f35c940bff54152d31535e2efb51e37
ClausPolanka <claus.polanka@chello.at>
2020-07-11 16:41:56 +0200

    Replace if with when to be more concise and extract constants

--- src/main/kotlin/pullrequestfactory/io/repositories/HeaderLinkLastPageParser.kt
+++ src/main/kotlin/pullrequestfactory/io/repositories/HeaderLinkLastPageParser.kt
@@ -4,13 +4,13 @@ class HeaderLinkLastPageParser {
 
     private val DEFAULT_VALUE = "-1"
     private val IDX_OF_MATCHED_VAL = 1
-    private val pattern = "page=([0-9]+)".toRegex()
+    private val PATTERN = "page=([0-9]+)".toRegex()
 
     fun last_page_of_branches(linkHeader: String?): String {
         return when (linkHeader) {
             null -> DEFAULT_VALUE
             else -> {
-                val matches = pattern.findAll(linkHeader)
+                val matches = PATTERN.findAll(linkHeader)
                 when {
                     matches.none() -> DEFAULT_VALUE
                     else -> matches.last().groupValues[IDX_OF_MATCHED_VAL]
