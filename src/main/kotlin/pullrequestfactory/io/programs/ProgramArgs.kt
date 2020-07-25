6a3dcf3240c30fbf3f72ae0be81887ead3043e80
ClausPolanka <claus.polanka@chello.at>
2020-07-25 11:41:12 +0200

    Extract methods and constants to seperate concerns

--- src/main/kotlin/pullrequestfactory/io/programs/ProgramArgs.kt
+++ src/main/kotlin/pullrequestfactory/io/programs/ProgramArgs.kt
@@ -11,6 +11,9 @@ class ProgramArgs(private val args: Array<String>) {
     private val OPEN_COMMAND = "open"
     private val IS_LAST_PULL_REQUEST_FINISHED = "-l"
     private val IS_LAST_PULL_REQUEST_FINISHED_LONG_VERSION = "--last-finished"
+    private val ERROR_MSG_CANDIDATE = "Either option -c or candidate missing or candidate has wrong format"
+    private val ERROR_MSG_PAIRING_PARTNER = "Either option -p or pairing partner are missing or pairing partner have wrong format"
+    private val ERROR_MSG_TOKEN = "Either option -g or github basic auth token missing"
 
     fun has_help_option() =
             args.isEmpty() || args.size == 1 && (args[0] == "-?" || args[0] == "--help")
@@ -42,18 +45,23 @@ class ProgramArgs(private val args: Array<String>) {
 
     fun has_close_command() = args[0] == CLOSE_COMMAND
 
+
     private fun has_close_command_required_options() = args.size == 5
             && is_candidate_syntax_valid()
             && is_github_basic_auth_token_syntax_valid()
 
     fun get_candidate(): Candidate {
-        if (!is_candidate_syntax_valid()) {
-            throw WrongCandidateArgumentSyntax("Either option -c or candidate missing or candidate has wrong format")
-        }
+        validate_args_candidate_syntax()
         val candidateArg = args[args.indexOf(CANDIDATE_OPTION) + 1]
         val firstName = candidateArg.split("-")[0]
         val lastName = candidateArg.split("-")[1]
         return Candidate(firstName, lastName)
+    }
+
+    private fun validate_args_candidate_syntax() {
+        if (!is_candidate_syntax_valid()) {
+            throw WrongCandidateArgumentSyntax(ERROR_MSG_CANDIDATE)
+        }
     }
 
     private fun is_candidate_syntax_valid() = args.size >= 2
@@ -61,22 +69,30 @@ class ProgramArgs(private val args: Array<String>) {
             && args[args.indexOf(CANDIDATE_OPTION) + 1].contains("-")
 
     fun get_github_basic_auth_token(): String {
-        if (!is_github_basic_auth_token_syntax_valid()) {
-            throw WrongGithubBasicAuthTokenArgumentSyntax("Either option -g or github basic auth token missing")
-        }
+        validate_args_token_syntax()
         val indexOfGithubToken = args.indexOf(GITHUB_BASIC_AUTH_TOKEN_OPTION) + 1
         return args[indexOfGithubToken]
+    }
+
+    private fun validate_args_token_syntax() {
+        if (!is_github_basic_auth_token_syntax_valid()) {
+            throw WrongGithubBasicAuthTokenArgumentSyntax(ERROR_MSG_TOKEN)
+        }
     }
 
     private fun is_github_basic_auth_token_syntax_valid() =
             args.size >= 2 && args.contains(GITHUB_BASIC_AUTH_TOKEN_OPTION)
 
     fun get_pairing_partner(): List<String> {
+        validate_args_pairing_partner_syntax()
+        val idx = args.indexOf(PAIRING_PARTNER_OPTION) + 1
+        return args[idx].split("-")
+    }
+
+    private fun validate_args_pairing_partner_syntax() {
         if (!is_pairing_partner_syntax_valid()) {
-            throw WrongPairingPartnerArgumentSyntax("Either option -p or pairing partner are missing or pairing partner have wrong format")
+            throw WrongPairingPartnerArgumentSyntax(ERROR_MSG_PAIRING_PARTNER)
         }
-        val indexOfPairingPartner = args.indexOf(PAIRING_PARTNER_OPTION) + 1
-        return args[indexOfPairingPartner].split("-")
     }
 
     private fun is_pairing_partner_syntax_valid() = args.size >= 2
