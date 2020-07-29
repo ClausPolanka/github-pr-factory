661ffb70d0d1d456225a8af63ce4c97ebf9e82d8
ClausPolanka <claus.polanka@chello.at>
2020-07-29 19:52:23 +0200

    Reorder methods to be readable from top to bottom

--- src/main/kotlin/pullrequestfactory/domain/branches/Branch.kt
+++ src/main/kotlin/pullrequestfactory/domain/branches/Branch.kt
@@ -2,9 +2,9 @@ package pullrequestfactory.domain.branches
 
 data class Branch(val name: String) {
 
-    fun parts() = name.split("_")
-
     fun iteration_nr(): Int = parts().dropLast(1).last().toInt()
+
+    fun parts() = name.split("_")
 
     fun pairing_partner(): String = parts().last()
 
