7babc9fd4a26403b32586e7b05db24a8934e6266
ClausPolanka <claus.polanka@chello.at>
2020-08-04 14:35:27 +0200

    Use raw string for open command help instead of duplicating println(...)

--- src/main/kotlin/pullrequestfactory/io/programs/impl/ShowOpenCommandHelpOutputProgram.kt
+++ src/main/kotlin/pullrequestfactory/io/programs/impl/ShowOpenCommandHelpOutputProgram.kt
@@ -5,24 +5,27 @@ import pullrequestfactory.io.programs.Program
 class ShowOpenCommandHelpOutputProgram : Program {
 
     override fun execute() {
-        println("Usage: github-pr-factory open [OPTION]")
-        println("")
-        println("Open new Github pull requests for a 2nd round hiring candidate")
-        println("")
-        println("Options:")
-        println("  -c\t\tThe candidate's first name and last-name separated by hyphen")
-        println("  -g\t\tYour Github basic authorization token")
-        println("  -p\t\tSeven pairing-partner names separated by hyphen")
-        println("    \t\t  Currently supported names:")
-        println("    \t\t  claus, berni, bernhard, nandor, dominik, mihai, lampe, shubi")
-        println("    \t\t  markus, tibor, christian, michal, tomas, peter, martin, john, andrej")
-        println("")
-        println("  -l, --last-finished\t\tMarks last iteration pull request with '[PR]' as finished")
-        println("")
-        println("Example:")
-        println("  github-pr-factory open -c firstname-lastname \\")
-        println("    -g 10238sadf08klasjdf098 \\")
-        println("    -p claus-berni-nandor-dominik-mihai-lampe-shubi")
+        val helpOutput = """
+        Usage: github-pr-factory open [OPTION]
+        
+        Open new Github pull requests for a 2nd round hiring candidate
+        
+        Options:
+          -c\t\tThe candidate's first name and last-name separated by hyphen
+          -g\t\tYour Github basic authorization token
+          -p\t\tSeven pairing-partner names separated by hyphen
+            \t\t  Currently supported names:
+            \t\t  claus, berni, bernhard, nandor, dominik, mihai, lampe, shubi
+            \t\t  markus, tibor, christian, michal, tomas, peter, martin, john, andrej
+        
+          -l, --last-finished\t\tMarks last iteration pull request with '[PR]' as finished
+        
+        Example:
+          github-pr-factory open -c firstname-lastname \\
+            -g 10238sadf08klasjdf098 \\
+            -p claus-berni-nandor-dominik-mihai-lampe-shubi
+        """
+        println(helpOutput.trimIndent())
     }
 
 }
