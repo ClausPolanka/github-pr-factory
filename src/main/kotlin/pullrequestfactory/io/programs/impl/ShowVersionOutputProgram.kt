54ec005e295cab2f47ebe3d64b9a9a10a1c6e81a
ClausPolanka <claus.polanka@chello.at>
2020-08-04 14:57:12 +0200

    Extract field to be consistent with other files

--- src/main/kotlin/pullrequestfactory/io/programs/impl/ShowVersionOutputProgram.kt
+++ src/main/kotlin/pullrequestfactory/io/programs/impl/ShowVersionOutputProgram.kt
@@ -4,8 +4,10 @@ import pullrequestfactory.io.programs.Program
 
 class ShowVersionOutputProgram : Program {
 
+    private val properties = FileProperties("app.properties")
+
     override fun execute() {
-        val projectVersion = FileProperties("app.properties").get_project_version()
+        val projectVersion = properties.get_project_version()
         println("github-pr-factory version $projectVersion")
     }
 
