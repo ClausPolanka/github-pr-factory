3a48b1a4c84da2d91a065cbac44f5b923e04ef07
ClausPolanka <claus.polanka@chello.at>
2020-08-04 14:33:33 +0200

    Use raw string for close command help instead of duplicating println(...)

--- src/main/kotlin/pullrequestfactory/io/programs/impl/ShowCloseCommandHelpOutputProgram.kt
+++ src/main/kotlin/pullrequestfactory/io/programs/impl/ShowCloseCommandHelpOutputProgram.kt
@@ -5,16 +5,19 @@ import pullrequestfactory.io.programs.Program
 class ShowCloseCommandHelpOutputProgram : Program {
 
     override fun execute() {
-        println("Usage: github-pr-factory close [OPTION]")
-        println("")
-        println("Close Github pull requests for a 2nd round hiring candidate")
-        println("")
-        println("Options:")
-        println("  -c\t\tThe candidate's first name and last-name separated by hyphen")
-        println("  -g\t\tYour Github basic authorization token")
-        println("")
-        println("Example:")
-        println("  github-pr-factory close -c firstname-lastname -g 10238sadf08klasjdf098")
+        val helpOutput = """
+        Usage: github-pr-factory close [OPTION]
+        
+        Close Github pull requests for a 2nd round hiring candidate
+        
+        Options:
+          -c\t\tThe candidate's first name and last-name separated by hyphen
+          -g\t\tYour Github basic authorization token
+        
+        Example:
+          github-pr-factory close -c firstname-lastname -g 10238sadf08klasjdf098
+        """
+        println(helpOutput.trimIndent())
     }
 
 }
