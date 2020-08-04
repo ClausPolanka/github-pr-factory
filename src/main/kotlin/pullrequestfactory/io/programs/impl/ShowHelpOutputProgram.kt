9680f1a13eba8e4a82e6a75d0689effb0a438312
ClausPolanka <claus.polanka@chello.at>
2020-08-04 14:31:32 +0200

    Use raw string instead of duplicating println(...)

--- src/main/kotlin/pullrequestfactory/io/programs/impl/ShowHelpOutputProgram.kt
+++ src/main/kotlin/pullrequestfactory/io/programs/impl/ShowHelpOutputProgram.kt
@@ -5,20 +5,23 @@ import pullrequestfactory.io.programs.Program
 class ShowHelpOutputProgram : Program {
 
     override fun execute() {
-        println("Usage: github-pr-factory [OPTION] COMMAND")
-        println("")
-        println("A tool which helps opening and closing Github pull requests")
-        println("for the George backend chapter 2nd round hirings.")
-        println("")
-        println("Options:")
-        println("  -?, --help\t\t\tPrint this help statement")
-        println("  -v, --version\t\t\tPrint version information and quit")
-        println("")
-        println("Commands:")
-        println("  open\t\tOpen Github pull requests")
-        println("  close\t\tClose Github pull requests")
-        println("")
-        println("Run 'github-pr-factory COMMAND --help' for more information on a command.")
+        val helpOutput = """
+        Usage: github-pr-factory [OPTION] COMMAND
+        
+        A tool which helps opening and closing Github pull requests
+        for the George backend chapter 2nd round hirings.
+        
+        Options:
+          -?, --help\t\t\tPrint this help statement
+          -v, --version\t\t\tPrint version information and quit
+        
+        Commands:
+          open\t\tOpen Github pull requests
+          close\t\tClose Github pull requests
+        
+        Run 'github-pr-factory COMMAND --help' for more information on a command.
+        """
+        println(helpOutput.trimIndent())
     }
 
 }
