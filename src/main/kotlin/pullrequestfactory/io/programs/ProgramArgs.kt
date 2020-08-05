2f4b9b41ce037cfa7c26686985dbdd44cf38f3dd
ClausPolanka <claus.polanka@chello.at>
2020-08-05 16:23:11 +0200

    Close command is considered to be valid in interactive mode

--- src/main/kotlin/pullrequestfactory/io/programs/ProgramArgs.kt
+++ src/main/kotlin/pullrequestfactory/io/programs/ProgramArgs.kt
@@ -46,13 +46,15 @@ class ProgramArgs(private val args: Array<String>) {
     fun has_close_command_help_option() =
             args[0] == CLOSE_COMMAND && args.size == 2 && args[1] == HELP_COMMAND_LONG_VERSION
 
-    fun has_invalid_close_command() = has_close_command() && !has_close_command_required_options()
+    fun has_invalid_close_command() = has_close_command() && !(has_close_command_required_options() || is_interactive_mode())
 
     fun has_close_command() = args[0] == CLOSE_COMMAND
 
     private fun has_close_command_required_options() = args.size == 5
             && is_candidate_syntax_valid()
             && is_github_basic_auth_token_syntax_valid()
+
+    private fun is_interactive_mode() = args.contains("-i") || args.contains("--interactive")
 
     fun get_candidate(): Candidate {
         validate_args_candidate_syntax()
