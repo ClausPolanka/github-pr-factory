752d6f6695da34684f181d94cd895fd2215d0208
ClausPolanka <claus.polanka@chello.at>
2020-08-05 16:07:15 +0200

    Remove unnecessary white spaces

--- src/main/kotlin/pullrequestfactory/io/programs/impl/ShowHelpOutputProgram.kt
+++ src/main/kotlin/pullrequestfactory/io/programs/impl/ShowHelpOutputProgram.kt
@@ -7,18 +7,18 @@ class ShowHelpOutputProgram : Program {
     override fun execute() {
         val helpOutput = """
         Usage: github-pr-factory [OPTION] COMMAND
-        
+
         A tool which helps opening and closing Github pull requests
         for the George backend chapter 2nd round hirings.
-        
+
         Options:
           -?, --help${"\t\t\t"}Print this help statement
           -v, --version${"\t\t\t"}Print version information and quit
-        
+
         Commands:
           open${"\t\t"}Open Github pull requests
           close${"\t\t"}Close Github pull requests
-        
+
         Run 'github-pr-factory COMMAND --help' for more information on a command.
         """
         println(helpOutput.trimIndent())
