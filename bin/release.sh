c7491068b88e826a54e8af68399255041425bd63
ClausPolanka <claus.polanka@chello.at>
2020-09-01 18:29:48 +0200

    Remove comments and add missing commit statement

--- bin/release.sh
+++ bin/release.sh
@@ -30,20 +30,19 @@ if [ $(git branch | grep '^*' | cut -c 2-) = 'master' ]; then
 
     # create a mainline release branch
     BRANCH='release/'$MAJOR.$MINOR.x
-    echo $BRANCH
+
     mvn release:branch -B -DbranchName=$BRANCH  \
         -DreleaseVersion=$MAJOR.$MINOR.0        \
         -DdevelopmentVersion=$MAJOR.$(expr $MINOR + 1).0-SNAPSHOT
 
-    # update minor and patch versions on the branch
     git checkout $BRANCH
 
     # perform release
     mvn release:prepare release:perform -B -DreleaseVersion=$MAJOR.$MINOR.0
-
     commitChangedPoms 'Updated master project version after release'
 
     git checkout master
+    commitChangedPoms 'Updated master dependency minor versions after release'
 else
     echo 'Performing interim release'
     mvn release:prepare release:perform -B
