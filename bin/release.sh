ae761432c42a994bda37921e851542bd5cccef9e
ClausPolanka <claus.polanka@chello.at>
2020-09-01 20:59:43 +0200

    Add changelog preparation to release script

--- bin/release.sh
+++ bin/release.sh
@@ -28,12 +28,38 @@ if [ $(git branch | grep '^*' | cut -c 2-) = 'master' ]; then
     MINOR=$(echo $VERSION|tr '.' ' '|awk '{print $2}')
     PATCH=$(echo $VERSION|tr '.' ' '|awk '{print $3}')
 
+    NEW_DEV_VERSION=$MAJOR.$(expr $MINOR + 1).0-SNAPSHOT
+
+    # Prepare Changelog for new release
+    GITHUB_COMPARE_URL="https:\/\/github\.beeone\.at\/George\/georgebackend\/compare"
+    RELEASE_DATE=$(date +"%Y-%m-%d")
+    PREVIOUS_RELEASE="release-$MAJOR\.$(expr $MINOR - 1)\.0"
+
+    sed -i "s/## \[$VERSION-SNAPSHOT\]/\
+## \[$NEW_DEV_VERSION\] \
+\n\n### Added       \
+\n\n### Changed     \
+\n\n### Fixed       \
+\n\n### Removed     \
+\n\n### Internal    \
+\n\n## \[$VERSION\] - $RELEASE_DATE/g" CHANGELOG.md
+
+    sed -i "s/\[$VERSION-SNAPSHOT\]: $GITHUB_COMPARE_URL\/$PREVIOUS_RELEASE\.\.\.master/\
+\[$NEW_DEV_VERSION\]: $GITHUB_COMPARE_URL\/release-$VERSION\.\.\.master\
+\n\[$VERSION\]: $GITHUB_COMPARE_URL\/$PREVIOUS_RELEASE\.\.\.release-$VERSION\
+/g" CHANGELOG.md
+
+    git add CHANGELOG.md
+    git commit -m "Release `$VERSION` in changelog"
+    git push
+    # End of Changelog preperation for new release
+
     # create a mainline release branch
     BRANCH='release/'$MAJOR.$MINOR.x
 
     mvn release:branch -B -DbranchName=$BRANCH  \
         -DreleaseVersion=$MAJOR.$MINOR.0        \
-        -DdevelopmentVersion=$MAJOR.$(expr $MINOR + 1).0-SNAPSHOT
+        -DdevelopmentVersion=$NEW_DEV_VERSION
 
     git checkout $BRANCH
 
