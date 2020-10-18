6955eeca56421e2d9a0f34d78e37d23e7784141d
ClausPolanka <claus.polanka@chello.at>
2020-10-18 20:53:42 +0200

    Extract method and improve edge case handling

--- src/main/kotlin/pullrequestfactory/io/uis/PairingPartnerUI.kt
+++ src/main/kotlin/pullrequestfactory/io/uis/PairingPartnerUI.kt
@@ -36,17 +36,21 @@ class PairingPartnerUI(private val ui: UI) {
     }
 
     private fun get_pairing_partner_idx_for(session: Int): Int {
-        var ppIdx = -1
-        var ppIdxCandidate: String? = null
-        while (ppIdx == -1) {
-            try {
-                ppIdxCandidate = ui.get_user_input(msg = "👉 Pairing Partner Session $session: ")
-                ppIdx = ppIdxCandidate.toInt()
-            } catch (e: NumberFormatException) {
-                ui.show("🤭 No pairing partner found for given index: '$ppIdxCandidate'")
-            }
+        var ppIdx = Int.MIN_VALUE
+        while (ppIdx == Int.MIN_VALUE) {
+            val ppIdxCandidate = ui.get_user_input(msg = "👉 Pairing Partner Session $session: ")
+            ppIdx = to_int(ppIdxCandidate)
         }
         return ppIdx
+    }
+
+    private fun to_int(ppIdxCandidate: String): Int {
+        try {
+            return ppIdxCandidate.toInt()
+        } catch (e: NumberFormatException) {
+            ui.show("🤭 No pairing partner found for given index: '$ppIdxCandidate'")
+        }
+        return Int.MIN_VALUE
     }
 
 }
