7b4b9f3581560dadb9851dbccd32716e0e1267d8
ClausPolanka <claus.polanka@chello.at>
2021-01-06 21:03:11 +0100

    issue-16 Fix auth token bug

--- src/main/kotlin/pullrequestfactory/io/programs/Programs.kt
+++ src/main/kotlin/pullrequestfactory/io/programs/Programs.kt
@@ -17,20 +17,27 @@ object Programs {
         val authTokenFromProps = userProps.get_github_auth_token()
         val pa = ProgramArgs(args, authTokenFromProps)
         val repoUrl = baseUrl + repoPath
-        val httpClient = KhttpClient(pa.get_github_auth_token())
-        val httpClientStats = KhttpClientStats(httpClient)
-        val githubApiClient = GithubAPIClient(httpClient, baseUrl)
         return when {
             pa.has_help_option() -> ShowHelpOutputProgram()
             pa.has_version_option() -> ShowVersionOutputProgram(appProps)
             pa.has_open_command_help_option() -> ShowOpenCommandHelpOutputProgram()
             pa.has_invalid_open_command() -> ShowInvalidOpenCommandOutputProgram()
             pa.has_open_command_in_interactive_mode() -> OpenPullRequestsProgramInteractiveMode(ui, baseUrl, repoUrl, authTokenFromProps)
-            pa.has_open_command() -> OpenPullRequestsProgram(ui, pa, repoUrl, githubApiClient, httpClientStats)
+            pa.has_open_command() -> {
+                val httpClient = KhttpClient(pa.get_github_auth_token())
+                val httpClientStats = KhttpClientStats(httpClient)
+                val githubApiClient = GithubAPIClient(httpClient, baseUrl)
+                OpenPullRequestsProgram(ui, pa, repoUrl, githubApiClient, httpClientStats)
+            }
             pa.has_close_command_help_option() -> ShowCloseCommandHelpOutputProgram()
             pa.has_invalid_close_command() -> ShowInvalidCloseCommandOutputProgram()
             pa.has_close_command_in_interactive_mode() -> ClosePullRequestsProgramInteractiveMode(ui, baseUrl, repoUrl, authTokenFromProps)
-            pa.has_close_command() -> ClosePullRequestsPrograms(ui, pa, repoUrl, githubApiClient, httpClientStats)
+            pa.has_close_command() -> {
+                val httpClient = KhttpClient(pa.get_github_auth_token())
+                val httpClientStats = KhttpClientStats(httpClient)
+                val githubApiClient = GithubAPIClient(httpClient, baseUrl)
+                ClosePullRequestsPrograms(ui, pa, repoUrl, githubApiClient, httpClientStats)
+            }
             else -> ShowHelpOutputProgram()
         }
     }
