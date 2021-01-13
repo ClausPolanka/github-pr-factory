e224a98c7b15537ba636cede870425b1cf611baf
ClausPolanka <claus.polanka@chello.at>
2021-01-13 19:37:14 +0100

    issue-29 Support OPEN command

--- src/main/kotlin/pullrequestfactory/MainAlternative.kt
+++ src/main/kotlin/pullrequestfactory/MainAlternative.kt
@@ -3,17 +3,23 @@ import com.github.ajalt.clikt.core.context
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.prompt
+import com.github.ajalt.clikt.parameters.options.transformValues
 import com.github.ajalt.clikt.sources.PropertiesValueSource
 import pullrequestfactory.domain.Candidate
+import pullrequestfactory.domain.PairingPartner
+import pullrequestfactory.io.GithubAPIClient
+import pullrequestfactory.io.programs.ProgramArgs
 import pullrequestfactory.io.programs.impl.FileAppProperties
+import pullrequestfactory.io.programs.impl.OpenPullRequestsProgram
 import pullrequestfactory.io.repositories.KhttpClient
 import pullrequestfactory.io.repositories.KhttpClientStats
+import pullrequestfactory.io.uis.ConsoleUI
 
 fun main(args: Array<String>) {
     val appProps = FileAppProperties("app.properties")
     val baseUrl = appProps.get_github_base_url()
     val repoPath = appProps.get_github_repository_path()
-    OpenCommand(baseUrl, repoPath).main(arrayOf("-fn", "Claus", "-ln", "Polanka", "-l"))
+    OpenCommand(baseUrl, repoPath).main(args)
 }
 
 class OpenCommand(
@@ -31,19 +37,19 @@ class OpenCommand(
     private val cfn by candidateFirstNameOption()
     private val cln by candidateLastNameOption()
     private val githubToken by gitHubAuthorizationTokenOption()
+    private val pairingPartner by pairingPartner()
 
     override fun run() {
-        echo("Hello $cfn $cln $githubToken $isLastFinished")
+        echo("Hello $cfn $cln $githubToken $isLastFinished $pairingPartner")
         val candidate = Candidate(cfn, cln)
         val httpClient = KhttpClientStats(KhttpClient(githubToken))
-
-//        OpenPullRequestsProgram(ConsoleUI(),
-//                baseUrl + repoPath,
-//                GithubAPIClient(httpClient, baseUrl),
-//                httpClient,
-//                isLastIterationFinished,
-//                candidate,
-//                pairingPartner).execute()
+        OpenPullRequestsProgram(ConsoleUI(),
+                baseUrl + repoPath,
+                GithubAPIClient(httpClient, baseUrl),
+                httpClient,
+                isLastFinished,
+                candidate,
+                pairingPartner!!).execute()
     }
 }
 
@@ -58,3 +64,20 @@ fun CliktCommand.candidateLastNameOption() =
 
 fun CliktCommand.gitHubAuthorizationTokenOption() =
         option("-g", "--github-token").prompt("GitHub Authorization Token")
+
+fun CliktCommand.pairingPartner() = option("-p", "--pairing-partner", help = "Please chose 7 of: ${PairingPartner.names()}")
+        .transformValues(7) {
+            create_pairing_partner("${it[0]}-${it[1]}-${it[2]}-${it[3]}-${it[4]}-${it[5]}-${it[6]}")
+        }
+
+private val ERROR_MSG_PAIRING_PARTNER = "Either option -p or pairing partner are missing or pairing partner have wrong format or is unknown"
+
+private fun create_pairing_partner(pairingPartner: String): List<PairingPartner> {
+    return pairingPartner.split("-").map { br ->
+        val pp = PairingPartner.value_of(br)
+        when (pp) {
+            null -> throw ProgramArgs.WrongPairingPartnerArgumentSyntax("$ERROR_MSG_PAIRING_PARTNER for given branch '$br'")
+            else -> pp
+        }
+    }
+}
