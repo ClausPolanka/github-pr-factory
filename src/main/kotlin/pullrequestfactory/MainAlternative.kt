a207d8e49bcc771f09776b96a456ca92ccf4046f
ClausPolanka <claus.polanka@chello.at>
2021-01-13 09:24:43 +0100

    issue-29 Reduce complexity by using Clikt's ValueSource

--- src/main/kotlin/pullrequestfactory/MainAlternative.kt
+++ src/main/kotlin/pullrequestfactory/MainAlternative.kt
@@ -1,10 +1,11 @@
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.context
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.prompt
+import com.github.ajalt.clikt.sources.PropertiesValueSource
 import pullrequestfactory.domain.Candidate
 import pullrequestfactory.io.programs.impl.FileAppProperties
-import pullrequestfactory.io.programs.impl.FileUserProperties
 import pullrequestfactory.io.repositories.KhttpClient
 import pullrequestfactory.io.repositories.KhttpClientStats
 
@@ -12,11 +13,7 @@ fun main(args: Array<String>) {
     val appProps = FileAppProperties("app.properties")
     val baseUrl = appProps.get_github_base_url()
     val repoPath = appProps.get_github_repository_path()
-
-    when (val authToken = get_token_from_user_properties()) {
-        null -> OpenCommand(baseUrl, repoPath).main(args)
-        else -> OpenCommandWhenTokenExists(baseUrl, repoPath, authToken).main(args)
-    }
+    OpenCommand(baseUrl, repoPath).main(arrayOf("-fn", "Claus", "-ln", "Polanka", "-l"))
 }
 
 class OpenCommand(
@@ -24,40 +21,21 @@ class OpenCommand(
         private val repoPath: String
 ) : CliktCommand(name = "open") {
 
-    private val isLastFinished by isLastIterationFinishedFlag()
-    private val cfn by candidateFirstNameOption()
-    private val cln by candidateLastNameOption()
-    private val token by gitHubAuthorizationTokenOption()
-
-    override fun run() {
-        echo("Hello $cfn $cln $token $isLastFinished")
-        val candidate = Candidate(cfn, cln)
-        val httpClient = KhttpClientStats(KhttpClient(token))
-
-//        OpenPullRequestsProgram(ConsoleUI(),
-//                baseUrl + repoPath,
-//                GithubAPIClient(httpClient, baseUrl),
-//                httpClient,
-//                isLastIterationFinished,
-//                candidate,
-//                pairingPartner).execute()
+    init {
+        context {
+            valueSource = PropertiesValueSource.from("user.properties")
+        }
     }
-}
-
-class OpenCommandWhenTokenExists(
-        private val baseUrl: String,
-        private val repoPath: String,
-        private val token: String
-) : CliktCommand(name = "open") {
 
     private val isLastFinished by isLastIterationFinishedFlag()
     private val cfn by candidateFirstNameOption()
     private val cln by candidateLastNameOption()
+    private val githubToken by gitHubAuthorizationTokenOption()
 
     override fun run() {
-        echo("Hello $cfn $cln $token $isLastFinished")
+        echo("Hello $cfn $cln $githubToken $isLastFinished")
         val candidate = Candidate(cfn, cln)
-        val httpClient = KhttpClientStats(KhttpClient(token))
+        val httpClient = KhttpClientStats(KhttpClient(githubToken))
 
 //        OpenPullRequestsProgram(ConsoleUI(),
 //                baseUrl + repoPath,
@@ -70,20 +48,13 @@ class OpenCommandWhenTokenExists(
 }
 
 fun CliktCommand.isLastIterationFinishedFlag() =
-        option(names = arrayOf("-l", "--last-finsished")).flag()
+        option("-l", "--last-finsished").flag()
 
 fun CliktCommand.candidateFirstNameOption() =
-        option(names = arrayOf("-fn", "--first-name")).prompt("Candidate First Name")
+        option("-fn", "--first-name").prompt("Candidate First Name")
 
 fun CliktCommand.candidateLastNameOption() =
-        option(names = arrayOf("-ln", "--last-name")).prompt("Candidate Last Name")
+        option("-ln", "--last-name").prompt("Candidate Last Name")
 
 fun CliktCommand.gitHubAuthorizationTokenOption() =
-        option(names = arrayOf("-g", "--github-token"))
-                .prompt("GitHub Authorization Token", default = get_token_from_user_properties())
-
-private fun get_token_from_user_properties(): String? {
-    val userProps = FileUserProperties("user.properties")
-    val authTokenFromProps = userProps.get_github_auth_token()
-    return authTokenFromProps
-}
+        option("-g", "--github-token").prompt("GitHub Authorization Token")
