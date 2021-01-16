3d2e916296f521768abd6e615fcc68fa05a38396
ClausPolanka <claus.polanka@chello.at>
2021-01-16 18:02:55 +0100

    issue-29 Remove unused abstraction

--- src/main/kotlin/pullrequestfactory/io/programs/AppProperties.kt
+++
@@ -1,7 +0,0 @@
-package pullrequestfactory.io.programs
-
-interface AppProperties {
-    fun get_github_base_url(): String?
-    fun get_project_version(): String?
-    fun get_github_repository_path(): String?
-}
