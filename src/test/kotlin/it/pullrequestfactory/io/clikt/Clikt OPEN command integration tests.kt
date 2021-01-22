e58f77bc7d171394d938a7052ae286d033c9742b
ClausPolanka <claus.polanka@chello.at>
2021-01-22 18:58:11 +0100

    issue-29 Reformat code

--- src/test/kotlin/it/pullrequestfactory/io/clikt/Clikt OPEN command integration tests.kt
+++ src/test/kotlin/it/pullrequestfactory/io/clikt/Clikt OPEN command integration tests.kt
@@ -74,13 +74,27 @@ class `Clikt OPEN command integration tests` {
                 "-pp7", "vaclav",
         ))
 
-        verify(PullRequest("Firstname Lastname Iteration 1 / Session 1 Markus [PR]", Branch("master"), Branch("firstname_lastname_iteration_1_markus")))
-        verify(PullRequest("Firstname Lastname Iteration 2 / Session 2 Berni [PR]", Branch("firstname_lastname_iteration_1_markus"), Branch("firstname_lastname_iteration_2_berni")))
-        verify(PullRequest("Firstname Lastname Iteration 3 / Session 3 Lukas [PR]", Branch("firstname_lastname_iteration_2_berni"), Branch("firstname_lastname_iteration_3_lukas")))
-        verify(PullRequest("Firstname Lastname Iteration 4 / Session 4 Jakub [PR]", Branch("firstname_lastname_iteration_3_lukas"), Branch("firstname_lastname_iteration_4_jakub")))
-        verify(PullRequest("Firstname Lastname Iteration 5 / Session 5 Peter [PR]", Branch("firstname_lastname_iteration_4_jakub"), Branch("firstname_lastname_iteration_5_peter")))
-        verify(PullRequest("Firstname Lastname Iteration 6 / Session 6 Christian [PR]", Branch("firstname_lastname_iteration_5_peter"), Branch("firstname_lastname_iteration_6_christian")))
-        verify(PullRequest("Firstname Lastname Iteration 7 / Session 7 Vaclav", Branch("firstname_lastname_iteration_6_christian"), Branch("firstname_lastname_iteration_7_vaclav")))
+        verify(PullRequest("Firstname Lastname Iteration 1 / Session 1 Markus [PR]",
+                Branch("master"),
+                Branch("firstname_lastname_iteration_1_markus")))
+        verify(PullRequest("Firstname Lastname Iteration 2 / Session 2 Berni [PR]",
+                Branch("firstname_lastname_iteration_1_markus"),
+                Branch("firstname_lastname_iteration_2_berni")))
+        verify(PullRequest("Firstname Lastname Iteration 3 / Session 3 Lukas [PR]",
+                Branch("firstname_lastname_iteration_2_berni"),
+                Branch("firstname_lastname_iteration_3_lukas")))
+        verify(PullRequest("Firstname Lastname Iteration 4 / Session 4 Jakub [PR]",
+                Branch("firstname_lastname_iteration_3_lukas"),
+                Branch("firstname_lastname_iteration_4_jakub")))
+        verify(PullRequest("Firstname Lastname Iteration 5 / Session 5 Peter [PR]",
+                Branch("firstname_lastname_iteration_4_jakub"),
+                Branch("firstname_lastname_iteration_5_peter")))
+        verify(PullRequest("Firstname Lastname Iteration 6 / Session 6 Christian [PR]",
+                Branch("firstname_lastname_iteration_5_peter"),
+                Branch("firstname_lastname_iteration_6_christian")))
+        verify(PullRequest("Firstname Lastname Iteration 7 / Session 7 Vaclav",
+                Branch("firstname_lastname_iteration_6_christian"),
+                Branch("firstname_lastname_iteration_7_vaclav")))
     }
 
     private fun branchesFor(pairingPartner: List<PairingPartner>, fn: String, ln: String): Array<Branch> {
