76f03ad3f704b975a32716c3853d2246c10f7ced
Claus Polanka <claus.polanka@chello.at>
2021-05-16 19:03:43 +0200

    Override descriptor to be able to compile

--- src/main/kotlin/pullrequestfactory/io/programs/impl/InstantSerializer.kt
+++ src/main/kotlin/pullrequestfactory/io/programs/impl/InstantSerializer.kt
@@ -3,6 +3,9 @@ package pullrequestfactory.io.programs.impl
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.time.Instant
@@ -10,6 +13,9 @@ import java.time.Instant
 @ExperimentalSerializationApi
 @Serializer(forClass = Instant::class)
 object InstantSerializer : KSerializer<Instant> {
+
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor(serialName = "Instant", kind = PrimitiveKind.LONG)
 
     override fun serialize(encoder: Encoder, value: Instant) {
         encoder.encodeString("${value.toEpochMilli()}")
